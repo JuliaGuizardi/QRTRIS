607d59ea21bdadd855dd6a54fc9c1334137aec48
JuliaGuizardi <ju_guizardi@hotmail.com>
2023-05-30 15:02:11 -0300

    Update function name

--- src/QRTRIS.jl
+++ src/QRTRIS.jl
@@ -13,51 +13,51 @@ using LinearAlgebra
 include("auxiliars.jl")
 
 """
-    LMFactorization(A, λ0)
+    QRupdate(A, λ0)
 
 Creates a mutable struct containing the QR factorization of 'A' and the value of 'λ0'.
 
 """
-mutable struct LMFactorization
+mutable struct QRupdate
     A
     λ0 ::Float64
-    function LMFactorization(A, λ0)
+    function QRupdate(A, λ0)
         qr!(A)
         new(A, λ0)
     end
 end
 
 """
-    update!(LMFact :: LMFactorization, λ::Float64)
+    update!(QRup :: QRupdate, λ::Float64)
 
 Calculates δ = λ_[k + 1] - λ_k and update the matrix using δ.
 """
-update!(LMFact :: LMFactorization, λ::Float64)= 
+update!(QRup :: QRupdate, λ::Float64)= 
 begin
-    if size(LMFact.A, 1) <= 2
+    if size(QRup.A, 1) <= 2
         error("ArgumentError : Method does not accept matrices smaller than 3 columns.")
     else
-	δ = λ - LMFact.λ0
-	delta_update!(LMFact.A, δ) 
-	LMFact.λ0 = λ
-	return LMFact
+	δ = λ - QRup.λ0
+	delta_update!(QRup.A, δ) 
+	QRup.λ0 = λ
+	return QRup
     end
 end
 
 """
-    LinearAlgebra.ldiv!(LMFact :: LMFactorization, b :: Vector{Float64}, x :: Vector{Float64})
+    LinearAlgebra.ldiv!(QRup, b, x)
 
 Solves the system A'Ax = b, considering 'A' an upper triangular matrix.
 """
-LinearAlgebra.ldiv!(LMFact, b, x)= 
+LinearAlgebra.ldiv!(QRup, b, x)= 
 begin
-    m, n = size(LMFact.A)
+    m, n = size(QRup.A)
     
-    solve_lower!(LMFact.A, b, x, n)
+    solve_lower!(QRup.A, b, x, n)
     
-    solve_upper!(LMFact.A, x, n)
+    solve_upper!(QRup.A, x, n)
     
     return x
 end
 
-export LMFactorization, update!
+export QRupdate, update!
